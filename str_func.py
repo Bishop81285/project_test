69ad14b73a0fb421c9e119e98ea83a997d8ed785
Artyom Guryanov <bishop81285@gmail.com>
2023-06-16 21:37:20 +0600

    add str_titl() in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_up(text):
     return text.upper()
+
+
+def str_titl(text):
+    """
+    First letters upped
+    :param text: incoming string
+    :return: modified text
+    """
+    return text.title()
